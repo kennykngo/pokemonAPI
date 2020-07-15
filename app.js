495030cddd3dcdebae9ab4815607e816d2293095
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-07-15 12:41:45 -0700

    updated app.js with new variables

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-console.log("hello wrold");
+// DOM Objects
+const mainScreen = document.querySelector(".main-screen");
 
-const resultFromFetch = fetch("https://pokeapi.co/api/vs/pokemon/1").then(
-  (res) => {
-    console.log(res);
-  }
-);
-console.log(resultFromFetch);
+const pokeName = document.querySelector(".poke-name");
+const pokeId = document.querySelector(".poke-id");
+const pokeFrontImage = document.querySelector(".poke-front-image");
+const pokeBackImage = document.querySelector(".poke-back-image");
+const pokeTypeOne = document.querySelector(".poke-type-one");
+const pokeTypeTwo = document.querySelector(".poke-type-two");
+const pokeWeight = document.querySelector(".poke-weight");
+const pokeHeight = document.querySelector(".poke-height");
+
+fetch("https://pokeapi.co/api/v2/pokemon/1")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    mainScreen.classList.remove("hide");
+    pokeName.textContent = data["name"];
+    pokeId.textContent = data["id"];
+    pokeWeight.textContent = data["weight"];
+    pokeHeight.textContent = data["height"];
+  });
